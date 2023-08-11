4fde5c6a939d2b0c3d35cbe15c7c77bd74bdb721
Bircan Sezgin <bircansezgin@outlook.com.tr>
2023-08-11 22:17:06 +0300

    Initial Commit

---
+++ AstroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AstroApp
+//
+//  Created by Bircan Sezgin on 11.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello Ataberk")
+    }
+
+
+}
+
